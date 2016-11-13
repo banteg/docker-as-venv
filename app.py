d9a746addcefcd4dc16046310423c1a2295189ee
banteg <banteeg@gmail.com>
2016-11-13 10:30:44 +0300

    add redis

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import hug
+from redis import StrictRedis
+
+redis = StrictRedis(host='redis')
 
 @hug.get('/')
 def hello(name='world'):
-    return {'greeting': f'hello {name}'}
+    return {
+        'greeting': f'hello {name}',
+        'visits': redis.incr('visits')
+    }
